9d85aeffd46b80f52a865cd2df3386c958af08a8
Alan Coomes <acoomes33@gmail.com>
2021-01-13 20:47:16 -0600

    updated home headers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
 import Nav from "./components/Nav";
 
 function App() {
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>This is my portfolio website</h1>
+      <h4>you can see my blogs and my projects I've been working on</h4>
+    </div>
+  );
+}
+
+export default Home;
